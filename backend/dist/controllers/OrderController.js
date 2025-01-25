ae70815a7cf9d9298c52717de714a4511eb6905a
Salah-Moftah <132005420+Salah-Moftah@users.noreply.github.com>
2025-01-25 13:28:36 +0200

    Update OrderController.ts

--- backend/dist/controllers/OrderController.js
+++ backend/dist/controllers/OrderController.js
@@ -132,7 +132,8 @@ const createSession = (lineItems, orderId, deliveryPrice, restaurantId) => __awa
             orderId,
             restaurantId,
         },
-        success_url: `${FRONTEND_URL}/order-status`,
+        // success_url: `${FRONTEND_URL}/order-status?success=true`,
+        success_url: `${FRONTEND_URL}`,
         cancel_url: `${FRONTEND_URL}/detail/${restaurantId}?cancelled=true`,
     });
     return sessionData;
